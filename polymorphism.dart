233c10788ca549c78726e8506e98b1424fc21b06
maahad1911 <maahad1911@gmail.com>
2025-08-09 22:47:47 +0500

    Update the code

--- polymorphism.dart
+++ polymorphism.dart
@@ -1,53 +1,36 @@
-void main() {
-  Hollywod obj1 = Hollywod("English", "Mahad", 19, "Premium", "Front");
-  obj1.detail();
-
-  print("\n------------------\n");
-
-  Bollywood obj2 = Bollywood("Urdu", "Areeba", 22, "Economy", "B12");
-  obj2.detail();
+void main () {
+  Animal animal = Animal();
+  animal.voice();
+  
+  Cat cat = Cat();
+  cat.voice();
+  
+  Cat cat1 = Cat();
+  cat.voice();
 }
 
-class Tickgen {
-  String? name;
-  int? age;
-  String? ticket;
-  String? seat;
+class Animal {
+  voice (){
+    print("There is lot of animals");
+    print("------------");
+  }
+  
+  
+}
 
-  Tickgen(this.name, this.age, this.ticket, this.seat);
-
-  detail() {
-    print(
-      "Thank you so much $name for coming. Kindly confirm your details.\nAge: $age\nClass: $ticket\nSeat: $seat",
-    );
+class Dog extends Animal{
+  @override
+  voice() {
+    print ("Dog is barking");
+    print("------------");
   }
 }
 
-class Hollywod extends Tickgen {
-  String? language;
-
-  // ✅ Don't use this.name in parameter list; use local vars and pass to super
-  Hollywod(String language, String name, int age, String ticket, String seat)
-      : this.language = language,
-        super(name, age, ticket, seat);
-
+class Cat extends Dog {
   @override
-  detail() {
-    super.detail();
-    print("🎬 Welcome to Hollywood Theater! Language: $language");
-  }
-}
-
-class Bollywood extends Tickgen {
-  String? language;
-
-  Bollywood(String language, String name, int age, String ticket, String seat)
-      : this.language = language,
-        super(name, age, ticket, seat);
-
-  @override
-  detail() {
-    super.detail();
-    print("🎥 Welcome to Bollywood Theater! Language: $language");
+  voice() {
+     print ("Cat is meowing");
+    super.voice();
+    print("------------");
   }
 }
